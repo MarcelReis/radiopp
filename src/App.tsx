2b39fb1f825e09d4968a0317b9c176d7312788b8
Marcelo Reis <oi@marcelreis.dev>
2020-08-23 18:54:02 -0300

    feat: basic header

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import "normalize.css";
 import PlayerContainer from "./container/PlayerContainer";
 import { RadioType } from "./api/radios";
 
+import AppBar from "./components/AppBar";
+
 function App() {
   const [radio, setRadio] = useState<null | RadioType>(null);
 
   return (
     <div className="App">
+      <AppBar />
       <div style={{ maxWidth: "800px", margin: "auto", padding: "16px" }}>
         <RadioContainer setRadio={setRadio} />
         <PlayerContainer radio={radio} />
