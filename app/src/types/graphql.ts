ae2bb13ce079c3fc957c4c65fd7e4a7875a0d90b
Marcelo Reis <oi@marcelreis.dev>
2020-08-30 18:24:58 -0300

    chore: update schema

--- app/src/types/graphql.ts
+++ app/src/types/graphql.ts
@@ -11,20 +11,44 @@ export type Scalars = {
   Float: number;
 };
 
+export type RadioDailySchedule = {
+  __typename?: "RadioDailySchedule";
+  time: Scalars["String"];
+  name?: Maybe<Scalars["String"]>;
+  presenter?: Maybe<Scalars["String"]>;
+};
+
+export type RadioSchedule = {
+  __typename?: "RadioSchedule";
+  sun?: Maybe<Array<RadioDailySchedule>>;
+  mon?: Maybe<Array<RadioDailySchedule>>;
+  tue?: Maybe<Array<RadioDailySchedule>>;
+  wed?: Maybe<Array<RadioDailySchedule>>;
+  thu?: Maybe<Array<RadioDailySchedule>>;
+  fri?: Maybe<Array<RadioDailySchedule>>;
+  sat?: Maybe<Array<RadioDailySchedule>>;
+};
+
 export type Radio = {
   __typename?: "Radio";
-  id?: Maybe<Scalars["Int"]>;
+  id: Scalars["Int"];
   name: Scalars["String"];
   originalURL: Scalars["String"];
   thumb: Scalars["String"];
   website: Scalars["String"];
-  streamURL: Scalars["String"];
+  streamURL?: Maybe<Scalars["String"]>;
   city: Scalars["String"];
   state: Scalars["String"];
   country: Scalars["String"];
+  schedule: RadioSchedule;
 };
 
 export type Query = {
   __typename?: "Query";
   radios: Array<Radio>;
+};
+
+export type QueryRadiosArgs = {
+  city?: Maybe<Scalars["String"]>;
+  limit?: Maybe<Scalars["Int"]>;
 };
