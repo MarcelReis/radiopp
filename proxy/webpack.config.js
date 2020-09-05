70e3da4b67ab44214ba6f92a216e78cdf3be2f72
Marcelo Reis <oi@marcelreis.dev>
2020-09-05 18:12:32 -0300

    chore: minify inline css

--- proxy/webpack.config.js
+++ proxy/webpack.config.js
@@ -5,7 +5,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: { filename: "main.js" },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+      },
+    }),
+  ],
   module: {
     rules: [
       {
